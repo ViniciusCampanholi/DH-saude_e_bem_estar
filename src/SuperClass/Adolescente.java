4caeea566b927cec3a2746d4b66340bb46ac9396
anamenici <anamenici@gmail.com>
2022-03-10 20:41:49 -0300

    Adição de parâmetros de IMC de adolescentes

--- src/SuperClass/Adolescente.java
+++ src/SuperClass/Adolescente.java
@@ -18,12 +18,197 @@ public class Adolescente extends Pessoa {
 	}
 	
 	public void classificarIMC(double imc) {
-		if(imc<25) {
-			System.out.println("Abaixo do peso");
-		}else {
-			System.out.println("Outro");
+		if (super.getGenero() == "Feminino")
+		{
+			if(super.getIdade()==10)
+			{
+				if(imc<=15)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=21)
+				{
+					System.out.println("Acima do peso");
+				}
+				
+			} 
+			else if(super.getIdade() ==11)
+			{
+				if(imc<=15)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=22)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+			else if(super.getIdade()==12)
+			{
+				if(imc<=16)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=23)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+			else if(super.getIdade()==13)
+			{
+				if(imc<=17)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=23)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+			else if(super.getIdade()==14)
+			{
+				if(imc<=17)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=24)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+			else if(super.getIdade()==15)
+			{
+				if(imc<=18)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=25)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+			else if(super.getIdade()==16)
+			{
+				if(imc<=18)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=25)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+			else if(super.getIdade()==17)
+			{
+				if(imc<=18)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=25)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+		else 
+		{
+			System.out.println("Peso normal");
 		}
-	}
+			
+			
+		if (getGenero() == "Masculino")
+		{
+			if(super.getIdade()==10)
+			{
+				if(imc<=15)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=20)
+				{
+					System.out.println("Acima do peso");
+				}
+			} 
+			else if(super.getIdade()==11)
+			{
+				if(imc<=15)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=20)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+			else if(super.getIdade()==12)
+			{
+				if(imc<=16)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=21)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+			else if(super.getIdade()==13)
+			{
+				if(imc<=16)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=22)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+			else if(super.getIdade()==14)
+			{
+				if(imc<=16)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=22)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+			else if(super.getIdade()==15)
+			{
+				if(imc<=17)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=23)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+			else if(super.getIdade()==16)
+			{
+				if(imc<=18)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=23)
+				{
+					System.out.println("Acima do peso");
+				}
+				}
+			else if(super.getIdade()==17)
+			{
+				if(imc<=18)
+				{
+					System.out.println("Abaixo do peso");
+				}
+				else if(imc>=23)
+				{
+					System.out.println("Acima do peso");
+				}
+			}
+		else 
+		{
+			System.out.println("Peso normal");
+		}
 	
-
+	}
 }
