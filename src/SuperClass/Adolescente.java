01e6ff0d633830bb9ddd353231cd1f7640d6eef5
anamenici <anamenici@gmail.com>
2022-03-11 17:17:45 -0300

    Limpeza de código 'Peso Normal' na classe Adolescente

--- src/SuperClass/Adolescente.java
+++ src/SuperClass/Adolescente.java
@@ -10,7 +10,7 @@ public class Adolescente extends Pessoa {
 
 	@Override
 	public String classificarIMC(double imc) {
-		String classe = "";
+		String classe = "Peso normal";
 
 		if (super.getGenero() == "Feminino") {
 			if (super.getIdade() == 10) {
@@ -18,8 +18,6 @@ public class Adolescente extends Pessoa {
 					classe = "Abaixo do peso";
 				} else if (imc >= 21) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 
 			} else if (super.getIdade() == 11) {
@@ -27,56 +25,42 @@ public class Adolescente extends Pessoa {
 					classe = "Abaixo do peso";
 				} else if (imc >= 22) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 12) {
 				if (imc <= 16) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 23) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 13) {
 				if (imc <= 17) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 23) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 14) {
 				if (imc <= 17) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 24) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 15) {
 				if (imc <= 18) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 25) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 16) {
 				if (imc <= 18) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 25) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 17) {
 				if (imc <= 18) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 25) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			}
 
@@ -88,56 +72,42 @@ public class Adolescente extends Pessoa {
 					classe = "Abaixo do peso";
 				} else if (imc >= 20) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 11) {
 				if (imc <= 15) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 20) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 12) {
 				if (imc <= 16) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 21) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 13) {
 				if (imc <= 16) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 22) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 14) {
 				if (imc <= 16) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 22) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 15) {
 				if (imc <= 17) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 23) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso Normal";
 				}
 			} else if (super.getIdade() == 16) {
 				if (imc <= 18) {
 					classe = "Abaixo do peso";
 				} else if (imc >= 23) {
 					classe = "Acima do peso";
-				} else {
-					classe = "Peso normal";
 				}
 			} else if (super.getIdade() == 17) {
 				if (imc <= 18) {
@@ -146,8 +116,6 @@ public class Adolescente extends Pessoa {
 					classe = "Acima do peso";
 				}
 
-			} else {
-				classe = "Peso normal";
 			}
 
 		}
--- src/SuperClass/main.java
+++ src/SuperClass/main.java
@@ -5,7 +5,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Adolescente adolescente = new Adolescente("Fulano", "Feminino", 5, 15, 1.69, 62);
+		Adolescente adolescente = new Adolescente("Fulano", "Masculino", 5, 15, 1.69, 62);
 		adolescente.imprimirRelatorio();
 		
 		Infantil infantil = new Infantil("Mariazinha", "Feminino", 1, 7, 1.35, 20);
