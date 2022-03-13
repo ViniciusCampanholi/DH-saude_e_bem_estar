d33c65e890db52dcb0eb5fca7b17b866976b8b8a
Guigaspaco <yoshiro2@gmail.com>
2022-03-13 19:41:56 -0300

    Infantil.java

--- src/SuperClass/Infantil.java
+++ src/SuperClass/Infantil.java
@@ -16,55 +16,93 @@ public class Infantil extends Pessoa {
 	
 	@Override
 	public String classificarIMC(double imc) {
-		String classe = "";
-		if(getIdade() == 6 ) {
-			if(imc >= 16.1) {
-				classe = "Acima do Peso";
+		String classe = "Peso normal";
+
+		if (super.getGenero() == "Feminino") {
+			if (super.getIdade() == 6) {
+				if (imc <= 16.1) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 17.4) {
+					classe = "Acima do peso";
+				}
+
+			} else if (super.getIdade() == 7) {
+				if (imc <= 17.1) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 18.9) {
+					classe = "Acima do peso";
+				}
+			} else if (super.getIdade() == 8) {
+				if (imc <= 18.1) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 20.3) {
+					classe = "Acima do peso";
+				}
+			} else if (super.getIdade() == 9) {
+				if (imc <= 19.1) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 21.7) {
+					classe = "Acima do peso";
+				}
+			} else if (super.getIdade() == 10) {
+				if (imc <= 20.1) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 23.2) {
+					classe = "Acima do peso";
+				}
+			} else if (super.getIdade() == 11) {
+				if (imc <= 21.1) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 24.5) {
+					classe = "Acima do peso";
+				}
+			
 			}
-			else {
-				classe = "Peso normal";
-			}
+
 		}
-		if(getIdade() == 7) {
-			if(imc >= 17.1) {
-				classe = "Acima do Peso";
+
+		if (getGenero() == "Masculino") {
+			if (super.getIdade() == 6) {
+				if (imc <= 16.6) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 18.0) {
+					classe = "Acima do peso";
+				}
+			} else if (super.getIdade() == 7) {
+				if (imc <= 17.3) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 19.1) {
+					classe = "Acima do peso";
+				}
+			} else if (super.getIdade() == 8) {
+				if (imc <= 16.7) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 20.3) {
+					classe = "Acima do peso";
+				}
+			} else if (super.getIdade() == 9) {
+				if (imc <= 18.8) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 21.4) {
+					classe = "Acima do peso";
+				}
+			} else if (super.getIdade() == 10) {
+				if (imc <= 19.6) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 22.5) {
+					classe = "Acima do peso";
+				}
+			} else if (super.getIdade() == 11) {
+				if (imc <= 20.3) {
+					classe = "Abaixo do peso";
+				} else if (imc >= 23.7) {
+					classe = "Acima do peso";
+				}
+			
+
 			}
-			else {
-				classe = "Peso normal";
-			}
-		}
-		if(getIdade() == 8) {
-			if(imc >= 18.1) {
-				classe = "Acima do Peso";
-			}
-			else {
-				classe = "Peso normal";
-			}
-		}
-		if(getIdade() == 9) {
-			if(imc >= 19.1) {
-				classe = "Acima do Peso";
-			}
-			else {
-				classe = "Peso normal";
-			}
-		}
-		if(getIdade() == 10) {
-			if(imc >= 20.1) {
-				classe = "Acima do Peso";
-			}
-			else {
-				classe = "Peso normal";
-			}
-		}
-		if(getIdade() == 11) {
-			if(imc >= 21.1) {
-				classe = "Acima do Peso";
-			}
-			else {
-				classe = "Peso normal";
-			}
+
 		}
 		return classe;
-	}	
+	}
 }
